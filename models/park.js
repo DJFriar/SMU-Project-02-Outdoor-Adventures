1212d6b99890356faf1f972efceb42bd853336ff
DJFriar <tommy.craft@icloud.com>
2021-01-16 14:25:44 -0600

    Updated the DB framework and seeds

--- models/park.js
+++ models/park.js
@@ -9,7 +9,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
-    state: {
+    parkid: {
       type: DataTypes.STRING,
       allowNull: false
     }
--- models/profile.js
+++ models/profile.js
@@ -5,7 +5,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false
     },
     parkID: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING,
       allowNull: false
     },
     isWishlist: {
