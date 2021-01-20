f3462d8911d82e1b950425f5873bcaf48d74e69a
DJFriar <tommy.craft@icloud.com>
2021-01-19 21:29:04 -0600

    resolved merge conflict

--- public/js/parks.js
+++ public/js/parks.js
@@ -1,27 +1,27 @@
 $(document).ready(() => {
   function parks() {
-    const apiKey = "3FZIVstmbfxjuxgM1Y85FFUTEClzCGY77bojFJtF";
+    const apiKey = "3FZIVstmbfxjuxgM1Y85FFUTEClzCGY77bojFJtF&limit=468";
     const queryUrl =
-      "https://developer.nps.gov/api/v1/parks?&api_key=" +
-      apiKey +
-      "&parkCode=yose";
+      "https://developer.nps.gov/api/v1/parks?&api_key=" + apiKey;
 
     $.ajax({
       url: queryUrl,
       method: "GET"
     }).then(response => {
       console.log(response);
+      let i = 0;
+      for (i < response.length; i++; ) {
+        const name = response.data[i].fullName;
+        const designation = response.data[i].designation;
+        const parkId = response.data[i].parkCode;
+        console.log(parkId);
 
-      const name = response.data[0].fullName;
-      const designation = response.data[0].designation;
-      const parkId = response.data[0].parkCode;
-      console.log(parkId);
-
-      $.post("/api/parks", {
-        name: name,
-        designation: designation,
-        parkid: parkId
-      });
+        $.post("/api/parks", {
+          name: name,
+          designation: designation,
+          parkid: parkId
+        });
+      }
     });
   }
 
