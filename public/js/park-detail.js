39e06d90224c17f928ff9165fb69e8aab30599a5
Harrison Glover <harrison_glover@yahoo.com>
2021-01-23 14:01:43 -0600

    added random images to park detail page

--- public/js/park-detail.js
+++ public/js/park-detail.js
@@ -1,43 +1,18 @@
 /* eslint-disable no-unused-vars */
-$(document).ready(() => {
+// $(document).ready(() => {
 
-  const urlPath = window.location.pathname;
 
-  let targetPark = "";
+//   console.log("parksjs running");
+//   const paths = ["/images/loginBackground.jpg","/images/loginBackground2.jpg","/images/loginBackground3.webp"
+  
+//   ];
+//   const arrayLength = paths.length-1;
+//   const randomGen = paths[Math.floor(Math.random() * arrayLength) + 1 ];
+//   console.log(randomGen);
+//   $(".profile-background").css({
+//     "background-image": "url('" + randomGen + "')",
+  
+//   });
 
-  function fetchParkFromURL() {
-    const urlPath = window.location.pathname;
-    const trimmedPath = urlPath.substring(7,11);
-    targetPark = trimmedPath;
-  }
 
-  console.log(targetPark);
-
-  function parks() {
-    const apiKey = "3FZIVstmbfxjuxgM1Y85FFUTEClzCGY77bojFJtF&limit=468";
-    const queryUrl =
-      "https://developer.nps.gov/api/v1/parks?&api_key=" + apiKey;
-
-    $.ajax({
-      url: queryUrl,
-      method: "GET"
-    }).then(response => {
-      console.log(response);
-      let i = 0;
-      for (i < response.length; i++; ) {
-        const name = response.data[i].fullName;
-        const designation = response.data[i].designation;
-        const parkId = response.data[i].parkCode;
-        console.log(parkId);
-
-        $.post("/api/parks", {
-          name: name,
-          designation: designation,
-          parkid: parkId
-        });
-      }
-    });
-  }
-
-  console.log("parksjs running");
-});
+// });
